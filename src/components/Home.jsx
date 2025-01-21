572f6df105d7db6812c106e0074172bd029c7eab
Franklin <antoniofranklin576@gmail.com>
2025-01-21 20:57:06 -0300

    :lipstick: feat: Estilizando textos e logo da Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,15 @@ const Home = () => {
   return (
     <div className="w-screen bg-[#1c130e]">
         <div className="flex justify-between items-center">
-            <div className="flex flex-col pl-8">
-                <img src={logo} alt="" className="w-20 absolute top-10"/>
-                <div className="text-white my-6 pl-4">
-                    <h1 className="text-6xl font-serif underline">Seja <br />Bem-vindo!</h1>
-                    <p className="font-sans pt-6 text-[#c4b394]">Vamos embarcar nessa aventura e <br />explorar esse mundo fantástico!</p>
+            <div className="flex flex-col pl-14">
+                <img src={logo} alt="" className="w-24 absolute top-10"/>
+                <div className="text-white mt-52 pl-4">
+                    <h1 className="text-7xl font-serif underline leading-tight">Seja <br />Bem-vindo!</h1>
+                    <p className="pt-8 text-[#c4b394] text-lg">Vamos embarcar nessa aventura e <br />explorar esse mundo fantástico!</p>
                 </div>
-                <span className="border-2 border-[#c4b394] w-[50px] ml-4"></span>
+                <span className="border-2 border-[#c4b399] w-[50px] ml-4 mt-12"></span>
             </div>
-            <img src="../src/assets/images/image.png" alt="" className="w-[650px] h-[600px] bg-cover"/>
+            <img src="../src/assets/images/image.png" alt="" className="w-[700px] h-[600px] object-cover"/>
         </div>
     </div>
   )
