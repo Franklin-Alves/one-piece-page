c40733c98aeb2e3d49858102d6b72ec16b0c3c68
Franklin <antoniofranklin576@gmail.com>
2025-02-02 11:28:26 -0300

    :sparkles: feat: Definindo a lógica para a mudança dinâmica dos personagens

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,59 @@
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { div, image, title } from 'motion/react-client'
+import React, { useState } from 'react'
+import { AnimatePresence, motion } from 'motion/react';
+
+const CharacterList = ({ charactersData }) => {
+
+  function nextCharacter(id) {
+    const newCharacter = charactersData.find(character => character.id > id) ? charactersData[id] : charactersData.find(character => character.id == 1)
+    setCharacterDetails(newCharacter)
+  }
+
+  function prevCharacter(id) {
+    const newCharacter = charactersData.find(character => character.id < id) ? charactersData[id - 2] : charactersData.find(character => character.id == charactersData.length)
+    setCharacterDetails(newCharacter)
+  }
+
+  const [characterDetails, setCharacterDetails] = useState(charactersData[0])
+
+  return (
+    <div className="flex justify-center items-center h-[550px]">
+      <div key={characterDetails.id}
+        className="flex">
+        <AnimatePresence mode='await'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.5,
+              scale: { type: "spring", visualDuration: 0.4, bounce: 0.4 }
+            }}
+            className="flex justify-center gap-28"
+          >
+            <img src={characterDetails.image} alt="" className="w-[300px] rounded-xl" />
+
+            <div className="w-1/2 text-[#062439] flex flex-col justify-center items-center">
+              <h1 className="text-8xl font-title font-bold">{characterDetails.name}</h1>
+              <span className="space-x-6 border-2 border-[#c4b599] w-[50px] mt-4"></span>
+              <h3 className="underline max-w-[300px] font-serif text-2xl text-center font-bold p-4">"{characterDetails.title}"</h3>
+              <p className="max-w-[300px] font-serif text-justify">{characterDetails.description}</p>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+
+        <div className="flex justify-center items-center">
+          <motion.button onClick={() => prevCharacter(characterDetails.id)} className="absolute left-20">
+            <ChevronLeft color="#30251e" size={74} strokeWidth={1} />
+          </motion.button >
+          <motion.button onClick={() => nextCharacter(characterDetails.id)} className="absolute right-16">
+            <ChevronRight color="#30251e" size={74} strokeWidth={1} />
+          </motion.button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default CharacterList
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,34 +1,85 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react'
-import React from 'react'
+import CharacterList from './CharacterList';
 
 const Characters = () => {
+
+  const charactersData = [
+    {
+      id: 1,
+      image: "../src/assets/images/characters/luffy.png",
+      name: "Luffy",
+      title: "Eu me tornarei o rei dos Piratas.",
+      description: "Ao completar 17 anos, Luffy parte em uma jornada do mar do East Blue para encontrar um tesouro lendário, o One Piece.",
+    },
+    {
+      id: 2,
+      image: "../src/assets/images/characters/zoro.jpg",
+      name: "Zoro",
+      title: "Eu nunca vou perder de novo!!",
+      description: "Roronoa Zoro é o espadachim dos Chapéus de Palha em One Piece, conhecido por sua habilidade com três espadas e sua determinação inabalável.",
+    },
+    {
+      id: 3,
+      image: "../src/assets/images/characters/sanji.jpg",
+      name: "Sanji",
+      title: "Pessoas são o que são no coração!",
+      description: " O cozinheiro da tripulação, Sanji é um lutador que usa apenas as pernas para atacar, seguindo um código de cavalaria. sonha em encontrar o All Blue.",
+    },
+    {
+      id: 4,
+      image: "../src/assets/images/characters/nami.jpg",
+      name: "Nami",
+      title: "⁠Todo mundo é fraco quando hesita.",
+      description: "A navegadora dos Chapéus de Palha, é uma estrategista brilhante e especialista em meteorologia e cartografia. Embora tenha começado como uma ladra",
+    },
+    {
+      id: 5,
+      image: "../src/assets/images/characters/usopp.jpg",
+      name: "Usopp",
+      title: "Eu sou o capitão Usopp!",
+      description: "Cozinheiro e lutador de pernas afiadas, segue o código da cavalaria e busca o All Blue, um mar lendário.",
+    },
+    {
+      id: 6,
+      image: "../src/assets/images/characters/robin.jpg",
+      name: "Robin",
+      title: "Eu quero viver!",
+      description: "A arqueóloga do bando e única sobrevivente de Ohara, Robin é especialista em história antiga e busca descobrir a verdade sobre o Século Perdido.",
+    },
+    {
+      id: 7,
+      image: "../src/assets/images/characters/chopper.jpg",
+      name: "Chopper",
+      title: "Obrigado por me amarem!",
+      description: "Uma rena que comeu a Hito Hito no Mi, adquirindo inteligência e a habilidade de se transformar em diversas formas.",
+    },
+    {
+      id: 8,
+      image: "../src/assets/images/characters/brook.jpg",
+      name: "Brook",
+      title: "Eu morri, sabia?",
+      description: "O músico da tripulação, Brook é um esqueleto vivo que voltou da morte graças à Yomi Yomi no Mi.",
+    },
+    {
+      id: 9,
+      image: "../src/assets/images/characters/franky.jpg",
+      name: "Franky",
+      title: "Quer ser homem, então seja homem!",
+      description: "O excêntrico carpinteiro ciborgue, Franky é responsável por construir o Thousand Sunny, o navio dos Chapéus de Palha.",
+    },
+    {
+      id: 10,
+      image: "../src/assets/images/characters/jinbei.jpg",
+      name: "Jinbei",
+      title: "Deixe-me ser seu escudo, Luffy!",
+      description: "Um homem-peixe mestre do Karatê Homem-Peixe e ex-Shichibukai, Jinbe é o timoneiro dos Chapéus de Palha.",
+    }
+  ]
+
   return (
     <div className="w-screen bg-[#1c130e] flex justify-center pb-4 pt-8">
       <div className="w-[90%] h-[570px] bg-white rounded-3xl">
-        <div className="flex justify-center items-center h-[550px]">
-          <div className="flex justify-center gap-28">
+        <CharacterList charactersData={charactersData} />
 
-            <img src="../src/assets/images/characters/luffy.png" alt="" className="w-72 rounded-xl"/>
-
-            <div className="w-1/2 text-[#062439] flex flex-col justify-center items-center">
-              <h1 className="text-8xl font-title font-bold">Luffy</h1>
-              <span className="space-x-6 border-2 border-[#c4b599] w-[50px] mt-4"></span>
-              <h3 className="underline max-w-[300px] font-serif text-2xl text-center font-bold p-4">"Eu me tornarei o rei dos Piratas."</h3>
-              <p className="max-w-[300px] font-serif text-justify">Ao completar 17 anos, Luffy parte em uma jornada do mar do East Blue para encontrar um tesouro lendário, o One Piece.
-              </p>
-            </div>
-            
-          </div>
-          <div className="flex justify-center items-center">
-            <button className="absolute left-20">
-                <ChevronLeft color="#30251e" size={74} strokeWidth={1} />
-            </button >
-            <button className="absolute right-16">
-                <ChevronRight color="#30251e" size={74} strokeWidth={1} />
-            </button>
-          </div>
-          
-        </div>
       </div>
     </div>
   )
